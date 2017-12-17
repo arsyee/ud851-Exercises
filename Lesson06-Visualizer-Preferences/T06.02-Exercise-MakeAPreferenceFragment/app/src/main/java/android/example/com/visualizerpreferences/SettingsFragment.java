cb4e8024287b89a577ca6201421921a9e23ae919
arsyee <2476948+arsyee@users.noreply.github.com>
2017-12-17 22:40:40 +0100

    T06.02 - Show Bass setting added

--- Lesson06-Visualizer-Preferences/T06.02-Exercise-MakeAPreferenceFragment/app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
+++ Lesson06-Visualizer-Preferences/T06.02-Exercise-MakeAPreferenceFragment/app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
@@ -3,13 +3,11 @@ package android.example.com.visualizerpreferences;
 import android.os.Bundle;
 import android.support.v7.preference.PreferenceFragmentCompat;
 
-/**
- * Created by tibi on 2017-11-23.
- */
-
 public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
-
+        // COMPLETED (5) In SettingsFragment's onCreatePreferences method add the preference file using the
+        // addPreferencesFromResource method
+        addPreferencesFromResource(R.xml.pref_visualizer);
     }
 }
