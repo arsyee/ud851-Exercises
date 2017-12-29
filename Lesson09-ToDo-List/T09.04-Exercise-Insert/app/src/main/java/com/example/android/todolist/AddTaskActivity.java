c74cce00376dcac43af55b640faf46acbb842b7c
arsyee <2476948+arsyee@users.noreply.github.com>
2017-12-29 22:57:42 +0100

    T09.04 - solution

--- Lesson09-ToDo-List/T09.04-Exercise-Insert/app/src/main/java/com/example/android/todolist/AddTaskActivity.java
+++ Lesson09-ToDo-List/T09.04-Exercise-Insert/app/src/main/java/com/example/android/todolist/AddTaskActivity.java
@@ -16,10 +16,17 @@
 
 package com.example.android.todolist;
 
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Toast;
+
+import com.example.android.todolist.data.TaskContract;
 
 
 public class AddTaskActivity extends AppCompatActivity {
@@ -27,6 +34,7 @@ public class AddTaskActivity extends AppCompatActivity {
     // Declare a member variable to keep track of a task's selected mPriority
     private int mPriority;
 
+    EditText mDescription;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +43,7 @@ public class AddTaskActivity extends AppCompatActivity {
         // Initialize to highest mPriority by default (mPriority = 1)
         ((RadioButton) findViewById(R.id.radButton1)).setChecked(true);
         mPriority = 1;
+        mDescription = (EditText) findViewById(R.id.editTextTaskDescription);
     }
 
 
@@ -43,14 +52,22 @@ public class AddTaskActivity extends AppCompatActivity {
      * It retrieves user input and inserts that new task data into the underlying database.
      */
     public void onClickAddTask(View view) {
-        // Not yet implemented
-        // TODO (6) Check if EditText is empty, if not retrieve input and store it in a ContentValues object
+        // COMPLETED (6) Check if EditText is empty, if not retrieve input and store it in a ContentValues object
+        if (mDescription.getText().length() == 0) {
+            Toast.makeText(this, "Please fill description!", Toast.LENGTH_LONG).show();
+            return;
+        }
+        ContentValues values = new ContentValues();
+        values.put(TaskContract.TaskEntry.COLUMN_DESCRIPTION, mDescription.getText().toString());
+        values.put(TaskContract.TaskEntry.COLUMN_PRIORITY, mPriority);
 
-        // TODO (7) Insert new task data via a ContentResolver
+        // COMPLETED (7) Insert new task data via a ContentResolver
+        Uri resultUri = getContentResolver().insert(TaskContract.TaskEntry.CONTENT_URI, values);
 
-        // TODO (8) Display the URI that's returned with a Toast
+        // COMPLETED (8) Display the URI that's returned with a Toast
         // [Hint] Don't forget to call finish() to return to MainActivity after this insert is complete
-
+        Toast.makeText(this, resultUri.toString(), Toast.LENGTH_LONG).show();
+        finish();
     }
 
 
